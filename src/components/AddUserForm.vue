f4dfcca59ed8ccb923fa3bb30b10af5f521e5858
Nenad <nbursa@gmail.com>
2025-03-29 12:23:21 +0100

    chore: move to elementplus

--- src/components/AddUserForm.vue
+++ src/components/AddUserForm.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <input name="name" />
-    <input name="email" />
-    <input name="age" />
-  </div>
+  <el-form>
+    <el-form-item label="Name">
+      <el-input name="name" />
+    </el-form-item>
+
+    <el-form-item label="Email">
+      <el-input name="email" />
+    </el-form-item>
+
+    <el-form-item label="Age">
+      <el-input name="age" />
+    </el-form-item>
+  </el-form>
 </template>
+
+<script setup lang="ts"></script>
--- tests/AddUserForm.spec.ts
+++ tests/AddUserForm.spec.ts
@@ -1,9 +1,15 @@
 import { mount } from '@vue/test-utils'
 import AddUserForm from '@/components/AddUserForm.vue'
+import ElementPlus from 'element-plus'
 
 describe('AddUserForm', () => {
   it('renders form with name, email, and age fields', () => {
-    const wrapper = mount(AddUserForm)
+    const wrapper = mount(AddUserForm, {
+      global: {
+        plugins: [ElementPlus],
+      },
+    })
+
     expect(wrapper.find('input[name="name"]').exists()).toBe(true)
     expect(wrapper.find('input[name="email"]').exists()).toBe(true)
     expect(wrapper.find('input[name="age"]').exists()).toBe(true)
